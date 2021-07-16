2f539fc8640b979388ed64a27e638d2633bf7a91
Akshaykoushik06 <akshaykoushikhothur@gmail.com>
2021-07-16 20:43:53 +0530

    Completed chapter 3

--- mflix/db.py
+++ mflix/db.py
@@ -560,7 +560,24 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$email",
+                "count": {
+                    "$sum": 1
+                }
+            }
+        },
+        {
+            "$sort": {
+                "count": -1
+            }
+        },
+        {
+            "$limit": 20
+        }
+    ]
 
     rc = db.comments.read_concern # you may want to change this read concern!
     comments = db.comments.with_options(read_concern=rc)
--- migrations/movie_last_updated_migration.py
+++ migrations/movie_last_updated_migration.py
@@ -14,7 +14,7 @@ us. We just need to make sure the correct operations are sent to MongoDB!
 """
 
 # ensure you update your host information below!
-host = "mongodb://localhost:27017"
+host = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.0seyi.mongodb.net/myFirstDatabase"
 
 # don't update this information
 MFLIX_DB_NAME = "sample_mflix"
@@ -25,8 +25,8 @@ mflix = MongoClient(host)[MFLIX_DB_NAME]
 # checks that its type is a string
 # a projection is not required, but may help reduce the amount of data sent
 # over the wire!
-predicate = {"some_field": {"$some_operator": "some_expression"}}
-projection = None
+predicate = {"lastupdated": {"$exists": True}, "lastupdated": {"$type": "string"}}
+projection = {"lastupdated": 1, "_id": 1}
 
 cursor = mflix.movies.find(predicate, projection)
 
@@ -50,7 +50,7 @@ try:
     # the new ISODate() type
     bulk_updates = [UpdateOne(
         {"_id": movie.get("doc_id")},
-        {"$some_update_operator": {"some_field_to_update"}}
+        {"$set": {"lastupdated": movie.get("lastupdated")}}
     ) for movie in movies_to_migrate]
 
     # here's where the bulk operation is sent to MongoDB
